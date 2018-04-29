7a5f434a45dcdfd383335436268d3bedec2ef5cd
Gabriella Spain <gabrsp@gmail.com>
2018-04-29 16:49:37 -0400

    Fixed percent; added rounding and precision; combined ans and log into one

--- app/components/calculator-component.js
+++ app/components/calculator-component.js
@@ -5,8 +5,6 @@ export default Component.extend({
   currentTheme: service(),
   log: '',
   display: '0',
-  ans: '',
-  ansLog: '',
 
   actions: {
     entry (value) {
@@ -14,7 +12,7 @@ export default Component.extend({
        this.send('number', value);
       }
 
-      if (value === '+' || value === '-' || value === '/' || value === '*' || value === '%') {
+      if (value === '+' || value === '-' || value === '/' || value === '*') {
         this.send('operator', value);
       }
 
@@ -24,6 +22,10 @@ export default Component.extend({
 
       if (value === '+-') {
         this.send('negate');
+      }
+
+      if (value === '%') {
+        this.send('percent');
       }
 
       if (value === '=') {
@@ -36,9 +38,6 @@ export default Component.extend({
     },
     number (value) {
       let display = this.get('display');
-      let log = this.get('log');
-      let ans = this.get('ans');
-      let ansLog = this.get('ansLog');
 
       if (display === '0' || isNaN(display)) {
         display = '';
@@ -50,40 +49,25 @@ export default Component.extend({
 
       if (display.length < 12) {
         display += value;
-        
-        ans
-          ? ansLog += value
-          : log += value;
-
-        this.set('log', log);
-        this.set('ansLog', ansLog);
       }
 
       this.set('display', display);
     },
     decimal (value) {
       let display = this.get('display');
-      let log = this.get('log');
-      let ans = this.get('ans');
-      let ansLog = this.get('ansLog');
 
       if (!display.includes(value)) {
         display += value;
+      }
 
-        ans
-          ? ansLog += value
-          : log += value;
-
-        this.set('log', log);
-        this.set('ansLog', ansLog);
+      if (isNaN(display)) {
+        display = '0' + value;
       }
 
       this.set('display', display);
     },
     negate () {
       let display = this.get('display');
-      let ans = this.get('ans');
-      let ansLog = this.get('ansLog');
 
       if (!isNaN(display)) {
         display.includes('-')
@@ -96,79 +80,61 @@ export default Component.extend({
     operator (value) {
       let display = this.get('display');
       let log = this.get('log');
-      let ans = this.get('ans');
-      let ansLog = this.get('ansLog');
       let lastLogged = log[log.length-1];
-      let lastAnsLogged = ansLog[ansLog.length-1];
 
-      if (log !== '' || display !== '0') {
-        display = value;
+      if ((log !== '' || display !== '0') && !isNaN(display)) {
+        log += " " + display + " " + value;
+       }
 
-        if(!isNaN(lastLogged) || !isNaN(lastAnsLogged)) {
-          ans
-            ? ansLog += value
-            : log += value;
-        }
-
-        if (lastLogged !== '.' || lastAnsLogged !== '.') {
-          ans
-            ? ansLog = ansLog.substring(0, ansLog.length-1) + value
-            : log = log.substring(0, log.length-1) + value;
-        }
-
-        this.set('log', log);
-        this.set('ansLog', ansLog);
+      if(!isNaN(lastLogged)) {
+        log += " " + value;
       }
 
+      if (lastLogged !== '.') {
+        log = log.substring(0, log.length-1) + value;
+      }
+
+      this.set('log', log);
       this.set('display', value);
     },
-    round (value, precision) {
-      let shift = function (value, precision, reverseShift) {
-        if (reverseShift) {
-          precision = -precision;
-        }  
-        let numArray = ("" + value).split("e");
-        return +(numArray[0] + "e" + (numArray[1] ? (+numArray[1] + precision) : precision));
-      };
+    percent () {
+      let display = this.get('display');
 
-      let rounded = shift(Math.round(shift(value, precision, false)), precision, true);
-      this.set('round', rounded);
+      if (display !== '0') {
+        display = display + ' / 100';
+
+        this.set('display', display);
+        this.send('equals', display);
+      }
     },
     equals () {
       let display = this.get('display');
-      let log = this.get('log');
-      let ans = this.get('ans');
-      let ansLog = this.get('ansLog');
+      var log = this.get('log');
 
-      if ((display === '0' || !isNaN(display)) && log !== '') {
-        let evaluate;
-
-        if (ans) {
-          evaluate = Function('"use strict";return (' + ansLog + ')')();
+      if (display !== '0' || !isNaN(display)) {
+        if (log !== '') {
+          log += " " + display;
         } else {
-          evaluate = Function('"use strict";return (' + log + ')')();
+          log = display;
         }
+
+        let evaluate = Function('"use strict";return (' + log + ')')();
+        evaluate = Math.round(evaluate * 100) / 100;
 
         if (evaluate.toString().length > 12) {
-          this.send('round', evaluate, 2);
-          let roundedAnswer = this.get('round');
-          ans = roundedAnswer.toString();
+          let formattedAns = evaluate.toPrecision(11);
+          log = formattedAns.toString();
         } else {
-          ans = evaluate.toString();
+          log = evaluate.toString();
         }
-        
-        ansLog = ans;
 
-        this.set('ansLog', ansLog);
-        this.set('ans', ans);
-        this.set('display', ans);
+        this.set('display', log);
+        this.set('log', '');
       }
     },
     clear () {
       this.set('display', '0');
       this.set('log', '');
-      this.set('ans', '');
-      this.set('ansLog', '');
     }
   }
 });
