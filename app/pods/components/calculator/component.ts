21178f7857ad78c4880aa750e86c83420b593673
Gabriella Spain <gabrsp@gmail.com>
2020-09-26 02:01:33 -0400

    Test fix; minor refactoring

--- app/pods/components/calculator/component.ts
+++ app/pods/components/calculator/component.ts
@@ -13,125 +13,91 @@ export default class Calculator extends Component<CalculatorArgs> {
   @tracked display = '0';
 
   @action
-  entry(value: string) {
-    value === '+' || value === '-' || value === '/' || value === '*'
-      ? this.operator(value)
-    : value === '.'
-      ? this.decimal(value)
-    : value === '+-'
-      ? this.negate()
-    : value === '%'
-      ? this.percent()
-    : value === '='
-      ? this.equals()
-    : value === 'clear'
-      ? this.clear()
-      : this.number(value);
-  }
-
-  @action
-  number(value: string) {
-    let display = this.display;
-
-    if (display === '0' || isNaN(display)) {
-      display = '';
+  number(value: number) {
+    if (this.display === '0' || isNaN(parseFloat(this.display))) {
+      this.display = '';
     }
 
-    if (display === '-0') {
-      display = '-';
+    if (this.display === '-0') {
+      this.display = '-';
     }
 
-    if (display.length < 12) {
-      display += value;
+    if (this.display.length < 12) {
+      this.display += value;
     }
-
-    this.display = display;
   }
 
   @action
   decimal(value: string) {
-    let display = this.display;
-
-    if (!display.includes(value)) {
-      display += value;
+    if (!this.display.includes(value)) {
+      this.display += value;
     }
 
-    if (isNaN(display)) {
-      display = '0' + value;
+    if (isNaN(parseFloat(this.display))) {
+      this.display = '0' + value;
     }
-
-    this.display = display;
   }
 
   @action
   negate() {
-    let display = this.display;
-
-    if (!isNaN(display)) {
-      display.includes('-')
-        ? display = display.slice(1)
-        : display = '-' + display;
+    if (!isNaN(parseFloat(this.display))) {
+      this.display.includes('-')
+        ? this.display = this.display.slice(1)
+        : this.display = '-' + this.display;
     }
-
-    this.display = display;
   }
 
   @action
   operator(value: string) {
-    let display = this.display;
-    let log = this.log;
-    let lastLogged = log[log.length-1];
+    let lastLogged = this.log[this.log.length-1];
 
-    if (!isNaN(display)) {
-      log += " " + display + " " + value;
+    if (!isNaN(parseFloat(this.display))) {
+      this.log += " " + this.display + " " + value;
      }
 
-    if(!isNaN(lastLogged)) {
-      log += " " + value;
+    if(!isNaN(parseFloat(lastLogged))) {
+      this.log += " " + value;
     }
 
     if (lastLogged !== '.') {
-      log = log.substring(0, log.length-1) + value;
+      this.log = this.log.substring(0, this.log.length-1) + value;
     }
 
-    this.log = log;
     this.display = value;
   }
 
   @action
   percent() {
-    let display = this.display;
-    let log = this.log;
-
-    if (display !== '0' && !isNaN(display)) {
-      log = display + ' / 100';
-
+    if (this.display !== '0' && !isNaN(parseFloat(this.display))) {
+      this.log = this.log + " " + this.display + ' / 100';
       this.display = '';
-      this.log = log;
       this.equals();
     }
   }
 
   @action
   equals() {
-    let display = this.display;
-    let log = this.log;
+    if(!this.display && this.log) {
+      const evaluate = Function('"use strict";return (' + this.log + ')')();
+      this.display = evaluate.toString();
+      this.log = '';
+    }
 
-    if (!isNaN(display)) {
-      log !== ''
-        ? log += " " + display
-        : log = display;
+    if (this.display && !isNaN(parseFloat(this.display))) {
+      this.log !== ''
+        ? this.log += " " + this.display
+        : this.log = this.display;
 
-      let evaluate = Function('"use strict";return (' + log + ')')();
+      const evaluate = Function('"use strict";return (' + this.log + ')')();
 
       if (evaluate.toString().length > 12) {
         let formattedAns = evaluate.toPrecision(6);
-        log = formattedAns.toString();
+        this.log = formattedAns.toString();
       } else {
-        log = evaluate.toString();
+        this.log = evaluate.toString();
       }
 
-      this.display = log;
+      this.display = this.log;
       this.log = '';
     }
   }
